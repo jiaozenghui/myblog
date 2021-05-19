f3563ca479d04a616dd7c5bc77bbf4e5a31e5476
jiaozh1 <jiaozh1@lenovo.com>
2021-05-19 18:44:24 +0800

    update nz_timepicker

--- app/public/js/controllers/article.controller.js
+++ app/public/js/controllers/article.controller.js
@@ -81,7 +81,31 @@
         $scope.article = {
         };
         $scope.categories = [];
-
+        $scope.config={
+                //初始化编辑器内容
+                content : '',
+                //是否聚焦 focus默认为false
+                focus : true,
+                //首行缩进距离,默认是2em
+                indentValue:'2em',
+                //初始化编辑器宽度,默认1000
+                initialFrameWidth:'100%',
+                //初始化编辑器高度,默认320
+                initialFrameHeight:530,
+                //编辑器初始化结束后,编辑区域是否是只读的，默认是false
+                readonly : false ,
+                //启用自动保存
+                enableAutoSave: false,
+                //自动保存间隔时间， 单位ms
+                saveInterval: 500,
+                //是否开启初始化时即全屏，默认关闭
+                fullscreen : false,
+                //图片操作的浮层开关，默认打开
+                imagePopup:true,     
+                //提交到后台的数据是否包含整个html字符串
+                allHtmlEnabled:false,
+                functions :['map','insertimage','insertvideo','attachment','insertcode','template', 'background', 'wordimage']     
+        };
         var id = $stateParams.id;
         if (id) {
             var promise = $http({
@@ -90,31 +114,9 @@
             }).then(function (result) {
                 $scope.article = result.data.result;
                 //$scope.ueditorSetContent('editor', $scope.article.content);
-                $scope.config={
-                    //初始化编辑器内容
-                    content : $scope.article.content,
-                    //是否聚焦 focus默认为false
-                    focus : true,
-                    //首行缩进距离,默认是2em
-                    indentValue:'2em',
-                    //初始化编辑器宽度,默认1000
-                    initialFrameWidth:'100%',
-                    //初始化编辑器高度,默认320
-                    initialFrameHeight:530,
-                    //编辑器初始化结束后,编辑区域是否是只读的，默认是false
-                    readonly : false ,
-                    //启用自动保存
-                    enableAutoSave: false,
-                    //自动保存间隔时间， 单位ms
-                    saveInterval: 500,
-                    //是否开启初始化时即全屏，默认关闭
-                    fullscreen : false,
-                    //图片操作的浮层开关，默认打开
-                    imagePopup:true,     
-                    //提交到后台的数据是否包含整个html字符串
-                    allHtmlEnabled:false,
-                    functions :['map','insertimage','insertvideo','attachment','insertcode','template', 'background', 'wordimage']     
-                };
+                ue.ready(function () { 
+                    ue.SetContent($scope.article.content);
+                });
 
             }).catch(function (result) {
                 console.log(result)
