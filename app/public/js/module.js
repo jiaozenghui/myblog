812f633a94ede811623ed7d71b2c7bfcdeaf09e4
jiaozh1 <jiaozh1@lenovo.com>
2021-05-25 17:20:53 +0800

    Replenish

--- app/public/js/module.js
+++ app/public/js/module.js
@@ -10,12 +10,6 @@ var app = angular.module('app', [
         if (result) {
             $rootScope.user = result.data.result;
         }
-        if (result.data.success == false) {
-            if (result.data.errCode == 401) {
-                window.location = '/login';
-            }
-        }
-
     }).catch(function (result) {
         console.log(result)
     });
