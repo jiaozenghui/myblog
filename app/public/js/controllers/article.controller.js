8f392fed9951d5c8b80bec40d7d58924e7c2447f
jiaozh1 <jiaozh1@lenovo.com>
2021-06-04 10:22:51 +0800

    Replenish

--- app/public/js/controllers/article.controller.js
+++ app/public/js/controllers/article.controller.js
@@ -101,9 +101,6 @@
 
     }).controller('editController', function ($scope, $http, $stateParams, $rootScope) {
         /* var ue = UE.getEditor('editor'); */
-        if (!$rootScope.user) {
-            window.location = '/login';
-        }
         $scope.article = {
         };
         $scope.categories = [];
