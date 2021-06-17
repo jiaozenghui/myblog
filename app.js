c2175b41ec0af2442288bd7f0d46f77721959417
jiaozh1 <jiaozh1@lenovo.com>
2021-06-17 11:16:10 +0800

    measurement

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(session({
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser());
+/* app.use(bodyParser.json()); */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
