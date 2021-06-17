a238043eb4f47828e49a2d1ffc621b9ba0bdfc39
jiaozh1 <jiaozh1@lenovo.com>
2021-06-17 16:03:32 +0800

    measurement

--- config/routes.js
+++ config/routes.js
@@ -70,6 +70,8 @@ module.exports= function (app) {
 			|| template =='drawing'
 			) {
 				Article.getList(req, function(response) {
+					console.log(req)
+					console.log(response)
 					if (response.success == true) {
 						renderData["articles"] =  response.result;
 						res.render('index',renderData);
