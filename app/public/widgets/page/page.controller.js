4378b4a60b1060ae559bb19e0e51c3b68a6831be
jiaozh1 <jiaozh1@lenovo.com>
2021-06-29 15:17:07 +0800

    oi gtrafana

--- app/public/widgets/page/page.controller.js
+++ app/public/widgets/page/page.controller.js
@@ -35,12 +35,6 @@
     
             link: function( scope, element ){
                 var context = scope;
-                context.goTo = goTo;
-                context.goToNext = goToNext;
-                context.goToPre = goToPre;
-                context.goToBegin = goToBegin;
-                context.goToEnd = goToEnd;
-                context.jump = jump;
     　　　　　　　init();
     　　　　　　　function init(){
     　　　　　　　　 context.pageNumber = Math.ceil(context.total/context.pageCount);
@@ -69,61 +63,6 @@
                     context.minPageIndex = Math.min(...context.pageList);
                 }
 
-                function goTo(page) {
-                    context.pageIndex = page;
-                    context.onClickPage()(page);
-                    let currentMaxPage = Math.max(...context.pageList, page);
-                    let currentMinPage = Math.min(...context.pageList, page);
-                    if (currentMaxPage < Math.ceil(context.total/context.pageCount) && page == currentMaxPage) {
-                        if ((page+ context.showPage) <= context.total) {
-                            for(let i=page; i<=(page+context.showPage-1); i++) {
-                                context.pageList.push(i);
-                            }
-                            initialPageList(page, page+context.showPage-1);
-                        } else {
-                            for(let i=(context.total-context.showPage+1); i<=context.total; i++) {
-                                context.pageList.push(i);
-                            }
-                        }
-                    } if (currentMinPage>1 &&  page == currentMinPage) {
-                        context.pageList=[];
-                        if (page > context.showPage) {
-                            for(let i=(page-context.showPage+1); i<=page; i++) {
-                                context.pageList.push(i);
-                            }
-                        } else {
-                            for(let i=1; i<=context.showPage; i++) {
-                                context.pageList.push(i);
-                            }
-                        }
-                    }
-                    context.maxPageIndex = Math.max(...context.pageList);
-                    context.minPageIndex = Math.min(...context.pageList);
-                    
-                }
-                function jump(page) {
-                    window.location= changeURLArg(window.location.href, 'page', page)
-                }
-                function goToNext(page) {
-                    if (page < context.total) {
-                        context.pageIndex++;
-                        jump(context.pageIndex);
-                    }
-                }
-                function goToPre(page) {
-                    if (page > 1) {
-                        context.pageIndex--
-                        jump(context.pageIndex);
-                    }
-                }
-                function goToBegin() {
-                    context.pageIndex =1;
-                    jump(context.pageIndex);
-                }
-                function goToEnd() {
-                    context.pageIndex =Math.ceil(context.total/context.pageCount);
-                    jump(context.pageIndex);
-                }
             }
         };
     }
