b73d8529b3871c48897be18eefa3a428ad3d0480
jiaozh1 <jiaozh1@lenovo.com>
2021-07-01 11:10:30 +0800

    blog log

--- config/routes.js
+++ config/routes.js
@@ -28,71 +28,77 @@ module.exports= function (app) {
 
 	// static views
 	app.all('/*', function (req, res) {
-		Article.getStatistics(req, function(statics) {
-			var template ="articles";
-			var blog_title ="首页";
-			var renderData={statics: statics, type: ''};
-			if (req.url.indexOf('aboutme.html')>-1) {
-				template ="about";
-				blog_title ="关于我";
-			} else if (req.url.indexOf('qianduanjishu.html')>-1
-			|| req.url.indexOf('life_diary.html')>-1
-			|| req.url.indexOf('drawing.html')>-1) {
-				var tep_url = req.url.substring(req.url.lastIndexOf("/")+1);
-				var type =tep_url.substring(0, tep_url.indexOf("."));
-				req.query.type = type;
-				template = 'query_article';
-				renderData['category'] = type;
-				if (type == 'qianduanjishu') {
-					renderData['category_name'] = '前端技术';
-				} else if (type == 'life_diary') {
-					renderData['category_name'] = '慢生活';
-				} else {
-					renderData['category_name'] = '兴趣爱好';
-				}
-				blog_title =renderData['category_name'];
-
-			} else if (req.url.indexOf('search.html')>-1){
-				template = 'query_article';
-				renderData['category'] = 'search';
-				renderData['category_name'] = '搜索';
-				renderData['filter'] = req.query.filter;
-				blog_title =renderData['category_name']+ '-' + req.query.filter;
-
-			} else if (req.url.indexOf('articles/edit')>-1) {
-				template ="edit";
-			} else if (req.url.indexOf('articles/detail')>-1) {
-				var id_url = req.url.substring(req.url.lastIndexOf("/")+1);
-				var art_template = './articles/'+ id_url.substring(0, id_url.lastIndexOf("."));
-				template ="detail";
-				renderData['art_template'] = art_template;
-				renderData['type'] = 'detail';
-				renderData['article_id'] = id_url.substring(0, id_url.lastIndexOf("."));
-
-			}
-			renderData['template'] = template;
-			renderData['blog_title'] = blog_title;
-			if (template == "articles"
-			|| template =='query_article'
-			) {
-				Article.getList(req, function(response) {
-					if (response.success == true) {
-						renderData["articles"] =  response.result;
-						renderData["total"] =  response.total;
-						res.render('index',renderData);
+		try {
+			Article.getStatistics(req, function(statics) {
+				var template ="articles";
+				var blog_title ="首页";
+				var renderData={statics: statics, type: ''};
+				if (req.url.indexOf('aboutme.html')>-1) {
+					template ="about";
+					blog_title ="关于我";
+				} else if (req.url.indexOf('qianduanjishu.html')>-1
+				|| req.url.indexOf('life_diary.html')>-1
+				|| req.url.indexOf('drawing.html')>-1) {
+					var tep_url = req.url.substring(req.url.lastIndexOf("/")+1);
+					var type =tep_url.substring(0, tep_url.indexOf("."));
+					req.query.type = type;
+					template = 'query_article';
+					renderData['category'] = type;
+					if (type == 'qianduanjishu') {
+						renderData['category_name'] = '前端技术';
+					} else if (type == 'life_diary') {
+						renderData['category_name'] = '慢生活';
+					} else {
+						renderData['category_name'] = '兴趣爱好';
 					}
-				});
-			} else if(template == "detail"){
-				Article.getDetail(renderData['article_id'], function(re) {
-					renderData.statics.pv_total +=1 ;
-					renderData['blog_title'] = re.result.title;
+					blog_title =renderData['category_name'];
+	
+				} else if (req.url.indexOf('search.html')>-1){
+					template = 'query_article';
+					renderData['category'] = 'search';
+					renderData['category_name'] = '搜索';
+					renderData['filter'] = req.query.filter;
+					blog_title =renderData['category_name']+ '-' + req.query.filter;
+	
+				} else if (req.url.indexOf('articles/edit')>-1) {
+					template ="edit";
+				} else if (req.url.indexOf('articles/detail')>-1) {
+					var id_url = req.url.substring(req.url.lastIndexOf("/")+1);
+					var art_template = './articles/'+ id_url.substring(0, id_url.lastIndexOf("."));
+					template ="detail";
+					renderData['art_template'] = art_template;
+					renderData['type'] = 'detail';
+					renderData['article_id'] = id_url.substring(0, id_url.lastIndexOf("."));
+	
+				}
+				renderData['template'] = template;
+				renderData['blog_title'] = blog_title;
+				if (template == "articles"
+				|| template =='query_article'
+				) {
+					Article.getList(req, function(response) {
+						if (response.success == true) {
+							renderData["articles"] =  response.result;
+							renderData["total"] =  response.total;
+							res.render('index',renderData);
+						}
+					});
+				} else if(template == "detail"){
+					Article.getDetail(renderData['article_id'], function(re) {
+						renderData.statics.pv_total +=1 ;
+						renderData['blog_title'] = re.result.title;
+						res.render('index',renderData);
+					});
+				} else {
 					res.render('index',renderData);
-				});
-			} else {
-				res.render('index',renderData);
-				/* res.sendfile('index.html', {root: path.join(__dirname, 'app/views')}); */
-			}
-		});
+					/* res.sendfile('index.html', {root: path.join(__dirname, 'app/views')}); */
+				}
+			});
+		} catch (error) {
+			console.log(error)
+		}
+
+
 
 		
 	});
