1e984f1aeb0d11df920e37535f5c72a89454cd9c
jiaozh1 <jiaozh1@lenovo.com>
2021-07-01 13:47:36 +0800

    blog log

--- config/routes.js
+++ config/routes.js
@@ -29,6 +29,7 @@ module.exports= function (app) {
 	// static views
 	app.all('/*', function (req, res) {
 		try {
+			var a =1/0;
 			Article.getStatistics(req, function(statics) {
 				var template ="articles";
 				var blog_title ="首页";
@@ -95,7 +96,8 @@ module.exports= function (app) {
 				}
 			});
 		} catch (error) {
-			console.log(error)
+			console.log(error);
+			res.render('lost')
 		}	
 	});
 }
