9d1162967706427d95dd6b6a267d9be8ac04c9a3
jiaozh1 <jiaozh1@lenovo.com>
2021-07-01 13:54:17 +0800

    blog log

--- config/routes.js
+++ config/routes.js
@@ -29,7 +29,7 @@ module.exports= function (app) {
 	// static views
 	app.all('/*', function (req, res) {
 		try {
-			console.log(
+			throw new Error('Bootstrap\'s JavaScript requires jQuery')
 			Article.getStatistics(req, function(statics) {
 				var template ="articles";
 				var blog_title ="首页";
